72826f11d770dd146568fef671b0bdeac6fee14c
Marvin <marvin@kickner.bayern>
2025-07-26 12:05:52 +0200

    Deprecate `SavableActivity` and introduce `SavableActivitySerializable` and `SavableActivityJson`.
    The original `SavableActivity` is now deprecated. Two new abstract classes, `SavableActivitySerializable` and `SavableActivityJson`, have been introduced to provide more specific ways to handle saving and restoring activity state using either `Serializable` objects or JSON strings, respectively.
    
    Interfaces `ISavableActivitySerializable`, `ISavableActivityJson`, and `ISavableActivity` have been added to define the contract for these savable activities.

--- kotlin-extensions-android/src/main/java/bayern/kickner/kotlin_extensions_android/SavableActivity.kt
+++ kotlin-extensions-android/src/main/java/bayern/kickner/kotlin_extensions_android/SavableActivity.kt
@@ -2,7 +2,6 @@ package bayern.kickner.kotlin_extensions_android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import org.jetbrains.annotations.ApiStatus.Experimental
 import java.io.Serializable
 
 /**
@@ -10,8 +9,8 @@ import java.io.Serializable
  * Via [SavableActivity] and [getSavedData] objects can be stashed away and restored later. The objects must be serializable.
  * Via the variable [activityWasRecreated] one can find out if the activity was recreated (true) or not (false)
  */
-@Experimental
-class SavableActivity : ComponentActivity() {
+@Deprecated("Use SavableActivitySerializable or SavableActivityJson instead.")
+class SavableActivity<T : Serializable> : ComponentActivity() {
 
     private val savedData = HashMap<String, Serializable>()
     var activityWasRecreated = false
@@ -39,6 +38,67 @@ class SavableActivity : ComponentActivity() {
         outState.putBoolean("activityKilled", true)
     }
 
+}
+
+abstract class SavableActivitySerializable<T : Serializable> : ComponentActivity(), ISavableActivitySerializable<T> {
+
+    /**
+     * This field will be filled if the activity was recreated and you stored some data through [onSaveData]
+     */
+    var loadedSavableData: T? = null
+        private set
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loadedSavableData = savedInstanceState?.getSerializable("savable_serial") as? T
+        loadedSavableData?.let { onRestoreData(it) }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("savable_serial", onSaveData())
+    }
+}
+
+abstract class SavableActivityJson : ComponentActivity(), ISavableActivityJson<String> {
+    /**
+     * This field will be filled if the activity was recreated and you stored some data through [onSaveData]
+     */
+    var loadedSavableData: String? = null
+        private set
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        savedInstanceState?.getString("savable_json")?.let { rawJson ->
+            loadedSavableData = rawJson
+            loadedSavableData?.let { onRestoreData(it) }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("savable_json", onSaveData())
+    }
+}
+
+interface ISavableActivitySerializable<T : Serializable> : ISavableActivity<T>
+
+interface ISavableActivityJson<T> : ISavableActivity<T>
+
+interface ISavableActivity<T> {
+
+    /**
+     * Return all Data that need to be saved.
+     * That will be called when the activity is destroyed.
+     */
+    fun onSaveData(): T
+
+    /**
+     * Will be called when the activity is recreated and data was stored before.
+     *
+     * @param restoredData The data that was stored before through [onSaveData].
+     */
+    fun onRestoreData(restoredData: T)
 }
 
 
